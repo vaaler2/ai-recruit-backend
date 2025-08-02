77d990f14483ac064d269cafab10de0f09753e89
Valér <pintervaler2@gmail.com>
2025-08-02 23:34:25 +0200

    Fix webhook endpoint

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from fastapi import FastAPI, Request
 import logging
 
 app = FastAPI()
-
 logging.basicConfig(level=logging.INFO)
 
 @app.post("/webhook")
 async def handle_webhook(request: Request):
     try:
         data = await request.json()
-        logging.info("🔔 Webhook received!")
+        logging.info("✅ Webhook received!")
         logging.info(data)
         return {"status": "ok"}
     except Exception as e:
         logging.error(f"❌ Error: {e}")
         return {"status": "error", "detail": str(e)}
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=10000)
